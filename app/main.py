72c4c9036b163935ff0bfe751875bcf2491b2044
triineliskuum <triin-elis@ut.ee>
2026-03-17 10:39:00 +0200

    frontendi lisamine

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 from .database import Base, engine, SessionLocal
 from . import models
 from .models import Project
@@ -7,9 +10,22 @@ app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-def read_root():
-    return {"message": "Telia Project Assignment App is running"}
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+def show_form(request: Request):
+    db = SessionLocal()
+    try:
+        projects = db.query(Project).all()
+        return templates.TemplateResponse(
+            "project_assignment.html",
+            {
+                "request": request,
+                "projects": projects
+            }
+        )
+    finally:
+        db.close()
 
 @app.get("/projects")
 def get_projects():
